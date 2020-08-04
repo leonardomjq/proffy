74dfce98a75932bf96a0d51ba2809207e82261d0
leonjaques <leonardomjq@gmail.com>
2020-08-04 23:12:38 +0100

    first commit

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import PageHeader from "../../components/PageHeader";
+
+import "./styles.css";
+
+export default function TeacherList() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes sao os proffys disponiveis." />
+    </div>
+  );
+}
